8cbfb961e9b74b5615104916656554c52241f38c
Emerson Willian Danzer <136943768+ewdanzer@users.noreply.github.com>
2024-09-29 17:15:40 -0300

    Update exercicios_basicos_4.py

--- exercicios_basicos_4.py
+++ exercicios_basicos_4.py
@@ -1,4 +1,4 @@
-# 10. Frequência de k-mers em uma sequência de DNA
+# 1. Frequência de k-mers em uma sequência de DNA
 
 # K-mers são subsequências de comprimento fixo "k" extraídas de uma sequência biológica, como DNA ou RNA.
 # Eles são amplamente utilizados em bioinformática para análises de padrões, montagem de genomas e identificação de sequências repetitivas ou mutações.
@@ -15,7 +15,7 @@
 # CATG GCAT
 
 
-# 11. Percentual de identidade em alinhamento BLAST
+# 2. Percentual de identidade em alinhamento BLAST
 # Crie um programa que leia o arquivo de saída de um alinhamento BLAST (aqruivo resultado.txt), identifique a linha contendo o valor de identidade e o imprima na tela.
 
 # Exemplo de entrada:
@@ -25,7 +25,7 @@
 # Identidade: 21%
 
 
-# 12. Divisão de arquivo FASTA em múltiplos arquivos
+# 3. Divisão de arquivo FASTA em múltiplos arquivos
 # Escreva um programa que leia um arquivo FASTA contendo vários identificadores e suas respectivas sequências.
 # O programa deve criar arquivos separados para cada identificador, nomeando-os como o identificador seguido de .fasta.
 
@@ -43,7 +43,7 @@
 # Sequencia3.fasta
 
 
-# 13. Combinação de arquivos FASTA em um único arquivo
+# 4. Combinação de arquivos FASTA em um único arquivo
 # Faça um programa que receba uma lista de arquivos FASTA e combine todo o conteúdo desses arquivos em um único arquivo de saída chamado all.fasta.
 # O novo arquivo deve conter todos os identificadores e suas sequências.
 
@@ -56,7 +56,7 @@
 # all.fasta
 
 
-# 14. Conversão de formato de aminoácidos
+# 5. Conversão de formato de aminoácidos
 # Crie um programa que leia um arquivo contendo várias linhas, onde cada linha possui o nome de um aminoácido (no formato "nome", "sigla" ou "letra") e o formato desejado para conversão.
 # O programa deve retornar todas as conversões de aminoácidos para o formato solicitado.
 # A tabela de aminoácidos pode ser encontrada em: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4665559/table/diagnostics-04-00140-t001/
@@ -72,7 +72,7 @@
 # Y
 
 
-# 15. Conversão de arquivo PDB para FASTA
+# 6. Conversão de arquivo PDB para FASTA
 # Desenvolva um programa que leia um arquivo no formato PDB, extraia as sequências de aminoácidos de cada cadeia polipeptídica e as converta para o formato FASTA.
 # O programa deve gerar um arquivo de saída onde cada identificador corresponde ao código da cadeia extraída.
 
